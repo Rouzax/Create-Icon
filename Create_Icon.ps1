d3ca97e8771ffefd81b641e7a0abcc07fb416c80
Rouzax <github@mgdn.nl>
2023-01-19 11:22:19 +0100

    Pickup all svg images in root folder

--- Create_Icon.ps1
+++ Create_Icon.ps1
@@ -1,24 +1,29 @@
-param(
-    [Parameter(
-        mandatory = $true
-    )]
-    [string]    $ImagePath
-)
+# param(
+#     [Parameter(
+#         mandatory = $true
+#     )]
+#     [string]    $ImagePath
+# )
+
+$SVGImages = (Get-ChildItem -LiteralPath $PSScriptRoot -Filter '*.svg').FullName
 
 $Sizes = 256, 128, 64, 48, 32, 24, 16
 
 $InkScapePath = 'C:\Program Files\Inkscape\bin\inkscape.com'
 $ImgMagickPath = 'C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\magick.exe'
 
-$ImageRootPath = Split-Path -LiteralPath $ImagePath
-$ImageName = [System.IO.Path]::GetFileNameWithoutExtension($ImagePath) 
-New-Item -ItemType Directory -Force -Path $ImageRootPath\_TEMP | Out-Null
-
-foreach ($size in $Sizes)
-{
-    &$InkScapePath --export-type=png --export-overwrite --export-area-page --export-background-opacity=0 --export-filename=$ImageRootPath\_TEMP\$size.png -w $size -h $size $ImagePath
+foreach ($ImagePath in $SVGImages) {
+    <# $Image is the current item #>
+    $ImageRootPath = Split-Path -LiteralPath $ImagePath
+    $ImageName = [System.IO.Path]::GetFileNameWithoutExtension($ImagePath) 
+    New-Item -ItemType Directory -Force -Path $ImageRootPath\_TEMP | Out-Null
+    
+    foreach ($size in $Sizes)
+    {
+        &$InkScapePath --export-type=png --export-overwrite --export-area-page --export-background-opacity=0 --export-filename=$ImageRootPath\_TEMP\$size.png -w $size -h $size $ImagePath
+    }
+    
+    &$ImgMagickPath convert $ImageRootPath\_TEMP\256.png $ImageRootPath\_TEMP\128.png $ImageRootPath\_TEMP\64.png $ImageRootPath\_TEMP\48.png $ImageRootPath\_TEMP\32.png $ImageRootPath\_TEMP\24.png $ImageRootPath\_TEMP\16.png $ImageRootPath\$ImageName.ico
+    
+    Remove-Item -Force -Recurse -LiteralPath $ImageRootPath\_TEMP
 }
-
-&$ImgMagickPath convert $ImageRootPath\_TEMP\256.png $ImageRootPath\_TEMP\128.png $ImageRootPath\_TEMP\64.png $ImageRootPath\_TEMP\48.png $ImageRootPath\_TEMP\32.png $ImageRootPath\_TEMP\24.png $ImageRootPath\_TEMP\16.png $ImageRootPath\$ImageName.ico
-
-Remove-Item -Force -Recurse -LiteralPath $ImageRootPath\_TEMP
